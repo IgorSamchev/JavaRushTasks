291cf64dc26f803dabf8a6893ea039921e48ad57
IgorSamchev <38370921+IgorSamchev@users.noreply.github.com>
2018-04-21 08:04:39 +0300

    JavaRush tasks

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,69 @@
+package com.javarush.task.task24.task2413;
+
+import java.util.List;
+
+public class Arkanoid {
+    private int width;
+    private int height;
+    private Ball ball;
+    private Stand stand;
+    static Arkanoid game;
+
+    public void move(){
+
+    }
+
+    public void run(){
+        
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
+
+    private List<Brick> bricks;
+
+    public Arkanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+    }
+}
