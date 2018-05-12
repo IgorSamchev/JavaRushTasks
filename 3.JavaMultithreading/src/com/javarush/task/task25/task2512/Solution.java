66af07be7c40f0c2c801a1d00f477e6a03c6bd7e
IgorSamchev <38370921+IgorSamchev@users.noreply.github.com>
2018-05-12 12:00:52 +0300

    JavaRush tasks

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -7,8 +7,27 @@ public class Solution implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+        if (t != null) {
+            t.interrupt();
+        }
+        Throwable throwable = e.getCause();
+        if (throwable != null) {
+            uncaughtException(t, throwable);
+        }
+        System.out.println(e.getClass().getName() + ": " + e.getMessage());
     }
 
     public static void main(String[] args) {
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    throw new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI")));
+                } catch (Exception e) {
+                   getUncaughtExceptionHandler().uncaughtException(currentThread(), e);
+                }
+            }
+        };
+        thread.setUncaughtExceptionHandler(new Solution());
+        thread.start();
     }
 }
