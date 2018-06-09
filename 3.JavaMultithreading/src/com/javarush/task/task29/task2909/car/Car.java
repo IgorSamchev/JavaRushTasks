686e74becc781e1be3d5d36a3cc4758afc92ead4
IgorSamchev <38370921+IgorSamchev@users.noreply.github.com>
2018-06-10 00:13:47 +0300

    JavaRushTasks

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
@@ -2,7 +2,7 @@ package com.javarush.task.task29.task2909.car;
 
 import java.util.Date;
 
-public class Car {
+public abstract class Car {
     static public final int TRUCK = 0;
     static public final int SEDAN = 1;
     static public final int CABRIOLET = 2;
@@ -49,12 +49,9 @@ public class Car {
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
-        if (!isDriverAvailable())
-            return 0;
-        if (fuel <= 0)
-            return 0;
-
-        return numberOfPassengers;
+       if (canPassengersBeTransferred()){
+        return numberOfPassengers;}
+        else return 0;
     }
 
     public boolean isDriverAvailable() {
@@ -68,10 +65,8 @@ public class Car {
     public void startMoving() {
         if (numberOfPassengers > 0) {
             fastenPassengersBelts();
-            fastenDriverBelt();
-        } else {
-            fastenDriverBelt();
         }
+            fastenDriverBelt();
     }
 
     public void fastenPassengersBelts() {
@@ -80,13 +75,7 @@ public class Car {
     public void fastenDriverBelt() {
     }
 
-    public int getMaxSpeed() {
-        if (type == TRUCK)
-            return 80;
-        if (type == SEDAN)
-            return 120;
-        return 90;
-    }
+    public abstract int getMaxSpeed();
 
     public boolean isSummer(Date date , Date summerStart, Date summerEnd){
         if (date.before(summerStart) || date.after(summerEnd)){return false;}
@@ -99,5 +88,13 @@ public class Car {
 
     public double getSummerConsumption(int length){
         return length * summerFuelConsumption;
+    }
+
+    private boolean canPassengersBeTransferred(){
+       if (driverAvailable && fuel > 0){
+           return true;
+       } else {
+           return false;
+       }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Truck.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Truck.java
@@ -5,4 +5,10 @@ public class Truck extends Car {
     public Truck(int numberOfPassengers) {
         super(TRUCK, numberOfPassengers);
     }
+
+    @Override
+    public int getMaxSpeed() {
+        final int MAX_TRUCK_SPEED = 80;
+        return MAX_TRUCK_SPEED;
+    }
 }
