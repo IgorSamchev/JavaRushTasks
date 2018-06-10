33d9747ae5931832cbcf9a3c9c40ddd8c8d1ec3e
IgorSamchev <38370921+IgorSamchev@users.noreply.github.com>
2018-06-10 20:19:53 +0300

    JavaRushTasks

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
@@ -1,16 +1,31 @@
 package com.javarush.task.task26.task2601;
 
-/* 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/*
 Почитать в инете про медиану выборки
 */
 public class Solution {
 
     public static void main(String[] args) {
-String s;
+        Integer[] array = {13, 8, 15, 5, 17};
+        sort(array);
     }
 
     public static Integer[] sort(Integer[] array) {
-        //implement logic here
+        List<Integer> list = Arrays.asList(array);
+        Arrays.sort(array);
+
+        int mediane;
+        if (array.length % 2 == 0) {
+            mediane = (array[array.length / 2] + array[array.length / 2 - 1]) / 2;
+        } else {
+            mediane = array[array.length / 2];
+        }
+
+        Arrays.sort(array, Comparator.comparingInt(x -> Math.abs(mediane - x)));
         return array;
     }
 }
