93a7b21e3688193ae0373f7f9eeb611536553775
Shillien <igor.samtsev@tptlive.ee>
2019-05-13 20:39:44 +0300

    JR tasks

---
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -0,0 +1,22 @@
+package com.javarush.games.minesweeper;
+import com.javarush.engine.cell.*;
+
+public class MinesweeperGame extends Game {
+    private final static int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    
+    @Override
+   public void initialize(){
+         setScreenSize(SIDE, SIDE);
+         createGame();
+    }
+    
+    private void createGame(){
+        for(int y = 0; y < SIDE; y ++){
+            for(int x = 0; x < SIDE; x++){
+                gameField[y][x] = new GameObject(x,y);
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
+    }
+}
