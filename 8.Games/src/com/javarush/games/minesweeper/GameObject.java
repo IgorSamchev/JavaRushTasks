7a56b5ae6a4f5e37fb12a56f03c5287298eca540
Shillien <igor.samtsev@tptlive.ee>
2019-05-14 22:58:38 +0300

    MineSweeper

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -2,12 +2,16 @@ package com.javarush.games.minesweeper;
 
 
 public class GameObject {
+    public boolean isMine;
     public int x;
     public int y;
+    public int countMineNeighbors;
+
     
-    public GameObject(int x, int y){
+    public GameObject(int x, int y, boolean isMine){
         this.x = x;
         this.y = y;
-        a
+        this.isMine = isMine;
+
     }
 }
--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -1,22 +1,112 @@
 package com.javarush.games.minesweeper;
+
 import com.javarush.engine.cell.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MinesweeperGame extends Game {
     private final static int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
-    
-    @Override
-   public void initialize(){
-         setScreenSize(SIDE, SIDE);
-         createGame();
+    private int countMinesOnField;
+
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+        createGame();
     }
-    
-    private void createGame(){
-        for(int y = 0; y < SIDE; y ++){
-            for(int x = 0; x < SIDE; x++){
-                gameField[y][x] = new GameObject(x,y);
-                setCellColor(x, y, Color.ORANGE);
+
+    private void countMineNeighbors() {
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                if (!gameField[y][x].isMine){
+                    List<GameObject> list = getNeighbors(gameField[y][x]);
+                    for (GameObject go : list){
+                        if (go.isMine) gameField[y][x].countMineNeighbors++;
+                    }
+                }
+
             }
         }
+    }
+
+    private void createGame() {
+        int counter = 0;
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                boolean isMine = getRandomNumber(10) == 1;
+                gameField[y][x] = new GameObject(x, y, isMine);
+                setCellColor(x, y, Color.ORANGE);
+                if (isMine) counter++;
+            }
+        }
+        countMinesOnField = counter;
+        countMineNeighbors();
+    }
+
+    private List<GameObject> getNeighbors(GameObject gameObject) {
+        List<GameObject> list = new ArrayList<>();
+        int x = gameObject.x;
+        int y = gameObject.y;
+        if (x == 0) {
+            if (y == 0) {
+                list.add(gameField[y][x + 1]);
+                list.add(gameField[y + 1][0]);
+                list.add(gameField[y + 1][x + 1]);
+            } else if (y == SIDE - 1) {
+                list.add(gameField[y][x + 1]);
+                list.add(gameField[y - 1][x]);
+                list.add(gameField[y - 1][x + 1]);
+            } else {
+                list.add(gameField[y - 1][x]);
+                list.add(gameField[y - 1][x + 1]);
+                list.add(gameField[y][x + 1]);
+                list.add(gameField[y + 1][x]);
+                list.add(gameField[y + 1][x + 1]);
+            }
+        } else if (x == SIDE - 1) {
+            if (y == 0) {
+                list.add(gameField[y][x - 1]);
+                list.add(gameField[y + 1][x - 1]);
+                list.add(gameField[y + 1][x]);
+            } else if (y == SIDE - 1) {
+                list.add(gameField[y - 1][x - 1]);
+                list.add(gameField[y - 1][x]);
+                list.add(gameField[y][x - 1]);
+            } else {
+                list.add(gameField[y - 1][x - 1]);
+                list.add(gameField[y - 1][x]);
+                list.add(gameField[y][x - 1]);
+                list.add(gameField[y + 1][x]);
+                list.add(gameField[y + 1][x - 1]);
+            }
+        }
+        if (y == 0) {
+            if (x != 0 && x != SIDE - 1) {
+                list.add(gameField[y][x - 1]);
+                list.add(gameField[y][x + 1]);
+                list.add(gameField[y + 1][x - 1]);
+                list.add(gameField[y + 1][x]);
+                list.add(gameField[y + 1][x + 1]);
+            }
+        } else if (y == SIDE - 1) {
+            if (x != 0 && x != SIDE - 1) {
+                list.add(gameField[y - 1][x - 1]);
+                list.add(gameField[y - 1][x]);
+                list.add(gameField[y - 1][x + 1]);
+                list.add(gameField[y][x - 1]);
+                list.add(gameField[y][x + 1]);
+            }
+        }
+        if (x != 0 && x != SIDE - 1 && y != 0 && y != SIDE - 1) {
+            list.add(gameField[y - 1][x - 1]);
+            list.add(gameField[y - 1][x]);
+            list.add(gameField[y - 1][x + 1]);
+            list.add(gameField[y][x - 1]);
+            list.add(gameField[y][x + 1]);
+            list.add(gameField[y + 1][x - 1]);
+            list.add(gameField[y + 1][x]);
+            list.add(gameField[y + 1][x + 1]);
+        }
+        return list;
     }
 }
