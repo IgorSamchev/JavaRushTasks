c506215528ed6cfc8e80eb42fed7ad30e4571e36
Shillien <igor.samtsev@tptlive.ee>
2019-05-25 10:55:37 +0300

    JavaRushTasks

--- 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task31.task3112;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,13 +15,21 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         Path passwords = downloadFile("https://javarush.ru/testdata/secretPasswords.txt", Paths.get("D:/MyDownloads"));
 
-
         for (String line : Files.readAllLines(passwords)) {
             System.out.println(line);
         }
     }
 
     public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
-        // implement this method
+        URL url = new URL(urlString);
+        InputStream inputStream = url.openStream();
+
+        Path temp = Files.createTempFile("temp", ".tmp");
+        Files.copy(inputStream, temp);
+
+        String fileName = urlString.substring(urlString.lastIndexOf("/"));
+        Path result = Paths.get(downloadDirectory.toString(), fileName);
+        Files.move(temp, result);
+        return result;
     }
 }
